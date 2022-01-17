c34b2c0936ba9ff34c9de404601559b7629560cc
Tommy So <tommy8492nd@gmail.com>
2022-01-17 15:44:29 -0800

    Created basic Hello World response from applet

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+}
